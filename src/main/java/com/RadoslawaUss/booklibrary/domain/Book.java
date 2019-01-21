166ca73331792762196f7ea319657a0cc461d55a
RadoslawaUss <Radoslawa.Uss@santander.pl>
2019-01-21 13:15:04 +0100

    Commit
    stan po zajęcia 2

--- src/main/java/com/RadoslawaUss/booklibrary/domain/Book.java
+++ src/main/java/com/RadoslawaUss/booklibrary/domain/Book.java
@@ -1,23 +1,43 @@
 package com.RadoslawaUss.booklibrary.domain;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+import javax.persistence.*;
+import java.util.Random;
+
+//@Component
+//@Scope("prototype")//utworzą się 2 różne obiekty; zasięg - jak obiekt ma być tworzony - singleton cy może istnieć wiele instacji w kontekście
+
+@Entity
+//@Table(name = "book")
+
 public class Book {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)//automatycznie nadaje id
+    //@Column(name = "BookTitle")
+    private int id;
     private String title;
     private int year;
     private String publisher;
     private String isbn;
 
     public  Book(){
-        this.title = "Ogniem i mieczem";
-        this.year = 1972;
-        this.publisher = "Wydawca 123";
-        this.isbn = "ASD123123";
+        //this.title = "Ogniem i mieczem";
+        //this.year = new Random(). nextInt(2000);
+        //this.publisher = "Wydawca 123";
+        //this.isbn = "ASD123123";
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public Book(String title, int year, String publisher, String isbn) {
+        this.title = title;
+        this.year = year;
+        this.publisher = publisher;
+        this.isbn = isbn;
     }
 
     public void setTitle(String title) {
@@ -46,6 +66,15 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
