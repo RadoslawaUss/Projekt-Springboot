18ec8305fa4398194a0f760808fb8836034e4fcb
RadoslawaUss <Radoslawa.Uss@santander.pl>
2019-03-04 12:22:31 +0100

    commit;

---
+++ src/main/java/com/blackbeast/booklibrary/restcontrollers/ExceptionDetail.java
@@ -0,0 +1,31 @@
+package com.blackbeast.booklibrary.restcontrollers;
+
+import org.springframework.http.ResponseEntity;
+
+public class ExceptionDetail {
+    private String exceptionName;
+    private String message;
+
+
+
+    public ExceptionDetail(String exceptionName, String message) {
+        this.exceptionName = exceptionName;
+        this.message = message;
+    }
+
+    public String getExceptionName() {
+        return exceptionName;
+    }
+
+    public void setExceptionName(String exceptionName) {
+        this.exceptionName = exceptionName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/blackbeast/booklibrary/services/BookService.java
+++ src/main/java/com/blackbeast/booklibrary/services/BookService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -19,19 +20,19 @@ public class BookService {
     @Autowired
     AuthorRepository authorRepository;
 
-    public List<Book> getBooks(){
+    public List<Book> getBooks() {
         return new ArrayList<>(bookRepository.getBooks());
     }
 
     public void saveBook(Book book) {
-        if (book!=null){
+        if (book != null) {
             System.out.println("Zapisuję książkę o id:" + book.getId());
-            boolean bookExists = bookRepository.getBook(book.getId())!= null;
+            boolean bookExists = bookRepository.getBook(book.getId()) != null;
 
-            if(bookExists) {
+            if (bookExists) {
                 authorRepository.updateAuthor(book.getAuthor());
                 bookRepository.updateBook(book);
-            }else {
+            } else {
                 authorRepository.saveAuthor(book.getAuthor());
                 bookRepository.saveBook(book);
             }
@@ -39,14 +40,14 @@ public class BookService {
     }
 
     @Transactional
-    public void removeBook(int id){
+    public void removeBook(int id) {
         Book bookToRemove = bookRepository.getBook(id);
         Author authorToRemove = bookToRemove.getAuthor();
-       bookRepository.removeBook(bookRepository.getBook(id));
-       authorRepository.removeAuthor(authorToRemove);
+        bookRepository.removeBook(bookRepository.getBook(id));
+        authorRepository.removeAuthor(authorToRemove);
     }
 
-    public Book getBook(int id){
+    public Book getBook(int id) {
         return bookRepository.getBook(id);
     }
 
@@ -54,5 +55,22 @@ public class BookService {
         Book newBook = new Book();
         newBook.setAuthor(new Author());
         return newBook;
+    }
+
+    public List<Book> getBooksByAuthor(String authorName) {
+        if (authorName != null)
+            return new ArrayList(bookRepository.getBooksByAuthor(authorName));
+        else
+            return null;
+    }
+
+    public List<Book> getBooks(Integer year, String publisher, String isbn) {
+        return new ArrayList<>(bookRepository.getBooks(year,publisher,isbn));
+    }
+    public List<Book> getByTitle(String title) {
+        if (title != null)
+            return new ArrayList(bookRepository.getByTitle(title));
+        else
+            return null;
     }
 }
